b6827b5c82d09c1211e7a4af9d2a4bcc244c471c
iwanczakm <iwanczakm@gmail.com>
2016-07-21 16:20:36 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function startTime() {
     var months =['January', 'Febuary', 'March','April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December' ];
     var days = ['Sun', 'Mon' , 'Tue', 'Wed',  'Thu', 'Fri', 'Sat'];
     var curWeekDay = days[today.getDay()] ;
-    var curdDay = today.getDate();
+    var curDay = today.getDate();
     var curMonth = months[today.getMonth()];
     var curYear = today.getFullYear();
     var date = curWeekDay + "," + curDay + " " +curMonth+ " " curYear;
